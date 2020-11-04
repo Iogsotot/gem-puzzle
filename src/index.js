64896ce79c77ccbe56ca517467d87924fb3ef850
Iogsotot <ajustusa@gmail.com>
2020-11-04 20:42:25 +0300

    fix: add full webpack config, eslint, babel, scss etc

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-import '.styles/styles.css'
-import json from './assets/json'
+// import * as $ from 'jquery'
+// import json from './assets/json.json'
+// import xml from './assets/data.xml'
+// import csv from './assets/data.csv'
+// import WebpackLogo from '@/assets/webpack-logo.png'
+// import React from 'react'
+// import {render} from 'react-dom'
+// import './styles/styles.css'
+// import './styles/less.less'
+import './styles/style.scss'
+
+window.onclick(alert, "Пипа!")
